fea1c32dede96d029ec56fb0bd54a1392069bde9
James Fox <wumbo16@gmail.com>
2018-12-21 12:31:32 -0600

    Created Project
    The skeleton of the project was created and the project is set up to
    begin working on.

---
+++ src/Database_Manager/DatabseManager.java
@@ -0,0 +1,14 @@
+package Database_Manager;
+
+/*
+ * This class will handle the calls to the database and allow for updates within the program.
+ * This class should only be called to make calls to and from the database. Other classes will make these calls,
+ * but this class will handle the calls.
+ */
+public class DatabseManager {
+
+    // Default constructor for the database manager.
+    public DatabseManager() {
+
+    }
+}
---
+++ src/FootballPool.java
@@ -0,0 +1,5 @@
+public class FootballPool {
+    public static void main(String[] args) {
+
+    }
+}
