380e629cf2d28181f35fe54d4dd94928c70bf67e
James Fox <wumbo16@gmail.com>
2019-01-11 09:33:41 -0600

    Updates to UserProfile and HomePage GUI
    added comments and code to UserProfile and started working on the
    HomePage GUI.

--- src/GUI_Manager/HomePage.java
+++ src/GUI_Manager/HomePage.java
@@ -7,11 +7,14 @@ public class HomePage extends JFrame {
     private String username;
     private String userFirstName;
 
+    private JPanel buttonPannel;
+
     public HomePage(String title, String username) {
         this.setTitle(title);
     }
 
     public void createUI() {
-        
+        setSize(1200,700);
+
     }
 }
--- src/User_Manager/UserProfile.java
+++ src/User_Manager/UserProfile.java
@@ -2,9 +2,45 @@ package User_Manager;
 
 public class UserProfile {
 
+    private final int NULL_VALUE = -1;
+
     private User user;
+    private int weekPoints;
+    private int seasonPoints;
+    private int totalPoints;
+
 
     public UserProfile(User user) {
         this.user = user;
+        setWeekPoints();
+        setSeasonPoints();
+        setTotalPoints();
+    }
+
+    /*
+     * This method will call to the database to get the value of the users week points and will set the variable
+     * weekPoints to the value from the database for that user. If the user does not exist or cannot be found in the
+     * database the default NULL_VALUE will be the set value as -1.
+     */
+    private void setWeekPoints() {
+        this.weekPoints = NULL_VALUE;
+    }
+
+    /*
+     * This method will call to the database to get the value of the users week points and will set the variable
+     * seasonPoints to the value from the database for that user. If the user does not exist or cannot be found in the
+     * database the default NULL_VALUE will be the set value as -1.
+     */
+    private void setSeasonPoints() {
+        this.seasonPoints = NULL_VALUE;
+    }
+
+    /*
+     * This method will call to the database to get the value of the users week points and will set the variable
+     * totalPoints to the value from the database for that user. If the user does not exist or cannot be found in the
+     * database the default NULL_VALUE will be the set value as -1.
+     */
+    private void setTotalPoints() {
+        this.totalPoints = NULL_VALUE;
     }
 }
